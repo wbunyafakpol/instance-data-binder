bbd7fb2e0caa4073bc602239db2bdfadb0ffda78
ROGERPC\roger <rogeryudya@gmail.com>
2025-02-17 00:51:33 +0700

    init project and gitignore

---
+++ DevExp.CodeChallenge/Configurations/AwsSetting.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DevExp.CodeChallenge.Configurations;
+
+internal class AwsSetting
+{
+    public readonly string? AccessKey;
+    public readonly string? SecretKey;
+    public readonly string? Region;
+
+    public AwsSetting(IConfigurationRoot configurationRoot)
+    {
+        AccessKey = configurationRoot.GetValue<string>("Aws:ACCESS_KEY")?.ToString();
+        SecretKey = configurationRoot.GetValue<string>("Aws:SECRET_KEY")?.ToString();
+        Region = configurationRoot.GetValue<string>("Aws:Region")?.ToString();
+    }
+}
---
+++ DevExp.CodeChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using DevExp.CodeChallenge.Services;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Enter cloud provider (aws/azure/gcp):");
+        string? cloudProvider = Console.ReadLine();
+        Console.WriteLine("Enter instance id:");
+        string? instanceId = Console.ReadLine();
+
+        try
+        {
+            
+       
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
