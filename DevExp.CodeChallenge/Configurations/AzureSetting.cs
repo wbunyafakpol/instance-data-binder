480948bab6bbae99d72fc9c90fb90f8d614654e3
ROGERPC\roger <rogeryudya@gmail.com>
2025-02-17 01:12:47 +0700

    add gcp key and Azure Setting

---
+++ DevExp.CodeChallenge/Configurations/AzureSetting.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DevExp.CodeChallenge.Configurations;
+
+internal class AzureSetting
+{
+    public readonly string? ClientId;
+    public readonly string? ClientSecret;
+    public readonly string? TenantId;
+    public readonly string? SubscriptionId;
+
+    public AzureSetting(IConfigurationRoot configurationRoot)
+    {
+        ClientId = configurationRoot.GetValue<string>("Azure:ClientId")?.ToString();
+        ClientSecret = configurationRoot.GetValue<string>("Azure:ClientSecret")?.ToString();
+        TenantId = configurationRoot.GetValue<string>("Azure:TenantId")?.ToString();
+        SubscriptionId = configurationRoot.GetValue<string>("Azure:SubscriptionId")?.ToString();
+    }
+}
